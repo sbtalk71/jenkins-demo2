d735401267a5293aa4356f6a6b91d9ea9132df65
sbtalk71 <sbtalk@yahoo.com>
2022-06-09 10:31:06 +0530

    app.java updated

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Good World!");
 
 		Greet g = new Greet();
 		g.getGreeting();
